310fb8756e178a92b45d4a14efa18f674ef00247
pablo <titodawson@gmail.com>
2023-03-28 00:21:13 -0300

    download fix

--- download.py
+++ download.py
@@ -3,18 +3,20 @@
 
 import torch
 from diffusers.utils import load_image
-from diffusers import UniPCMultistepScheduler, StableDiffusionControlNetPipeline, ControlNetModel
+from diffusers import DPMSolverMultistepScheduler, StableDiffusionControlNetPipeline, ControlNetModel
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     path = {"midas": ["ckpt/dpt_hybrid-midas-501f0c75.pt","https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/dpt_hybrid-midas-501f0c75.pt"]}
     torch.hub.download_url_to_file(path["midas"][1], path["midas"][0])
 
-    controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-depth", torch_dtype=torch.float16)
+    controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-depth")
+    repo = "runwayml/stable-diffusion-v1-5"
+    scheduler = DPMSolverMultistepScheduler.from_pretrained(repo, subfolder="scheduler")
     model = StableDiffusionControlNetPipeline.from_pretrained(
-        "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
+        repo, controlnet=controlnet, safety_checker=None, scheduler=scheduler, torch_dtype=torch.float16, revision="fp16"
     )
-    
+
 
 if __name__ == "__main__":
     download_model()
